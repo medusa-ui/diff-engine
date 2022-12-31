7e215c9477e7f53409dcade70684ad58c577f297
Kevin Deyne <kevindeyne@gmail.com>
2022-12-30 16:01:34 -0800

    Added some failing complex tests

--- src/test/java/io/getmedusa/diffengine/complex/ComplexTest.java
+++ src/test/java/io/getmedusa/diffengine/complex/ComplexTest.java
@@ -179,6 +179,308 @@ class ComplexTest extends DiffEngineTest {
     }
 
     @Test
+    void testComplex8() {
+        String oldHTML = """
+        <body>
+            <section>
+            <div>
+                <p>If top is <code>true</code> this should be visible on <code>top</code> of the page</p>
+            </div>
+            <p>If middle is <code>true</code> this should be visible between <code>top</code> and <code>bottom</code></p>
+            <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code></p>
+            <div>
+                <button onclick="_M.doAction(event, '__FRAGMENT__', `change()`)">change</button>
+            </div>
+            <div>
+                <p>If top is <code>true</code> this should be visible on <code>top</code>, after the button</p>
+            </div>
+            <p>If middle is <code>true</code> this should be visible between <code>top</code> and <code>bottom</code>, after the button</p>
+            <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code>, after the button</p>
+            </section>
+        </body>
+        """;
+
+        final String newHTML = """
+        <body>
+            <section>
+                <div>
+                    <p>If top is <code>true</code> this should be visible on <code>top</code> of the page</p>
+                </div>
+                <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code></p>
+                <div>
+                    <button onclick="_M.doAction(event, '__FRAGMENT__', `change()`)">change</button>
+                </div>
+                <div>
+                    <p>If top is <code>true</code> this should be visible on <code>top</code>, after the button</p>
+                </div>
+                <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code>, after the button</p>
+            </section>
+        </body>
+        """;
+
+        Set<ServerSideDiff> diffs = engine.calculate(oldHTML, newHTML);
+        applyAndTest(oldHTML, newHTML, diffs);
+    }
+
+    @Test
+    void testComplex9() {
+        String oldHTML = """
+                        <!DOCTYPE html>
+                        <html lang="en">
+                         <head>
+                          <meta charset="UTF-8" />
+                          <title>Th If Problem?</title>
+                          <style>
+                                code {
+                                    color: crimson;
+                                    font-weight: bold;
+                                    padding: .25rem;
+                                }
+                            </style>
+                         </head>
+                         <body>
+                          <section>
+                           <div>
+                            <p>If top is <code>true</code> this should be visible on <code>top</code> of the page</p>
+                           </div>\s
+                           <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code></p>
+                           <div><button onclick="_M.doAction(event, '__FRAGMENT__', `change()`)">change</button>
+                           </div>
+                           <div>
+                            <p>If top is <code>true</code> this should be visible on <code>top</code>, after the button</p>
+                           </div>\s
+                           <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code>, after the button</p>
+                          </section>
+                         <div id="m-top-load-bar" class="progress-line" style="display:none;"></div>
+                        <style>
+                        div#m-top-load-bar {
+                            position: fixed;
+                            top: 0;
+                            left: 0;
+                            right: 0;
+                            width: 100%;
+                        }
+                        .progress-line, .progress-line:before {
+                            height: 3px;
+                            width: 100%;
+                            margin: 0;
+                        }
+                        .progress-line {
+                            background-color: #7a00ff;
+                            display: -webkit-flex;
+                            display: flex;
+                        }
+                        .progress-line:before {
+                            background-color: #f4abba;
+                            content: '';
+                            -webkit-animation: running-progress 2s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+                            animation: running-progress 2s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+                        }
+                        @-webkit-keyframes running-progress {
+                            0% { margin-left: 0px; margin-right: 100%; }
+                            50% { margin-left: 25%; margin-right: 0%; }
+                            100% { margin-left: 100%; margin-right: 0; }
+                        }
+                        @keyframes running-progress {
+                            0% { margin-left: 0px; margin-right: 100%; }
+                            50% { margin-left: 25%; margin-right: 0%; }
+                            100% { margin-left: 100%; margin-right: 0; }
+                        }
+                        </style>
+                        <div id="m-full-loader" style="display:none;">Loading ...</div>
+                        <style>
+                        div#m-full-loader {
+                            background: #0000006e;
+                            position: fixed;
+                            top: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 100%;
+                            text-align: center;
+                            padding-top: 15%;
+                        }
+                        </style>
+                        <template id="m-template-button-load">
+                            <span class="m-button-loader"><span></span><span></span><span></span><span></span></span>
+                        </template>
+                        <style>
+                                .m-button-loader {
+                                    display: inline-block;
+                                    position: relative;
+                                    width: 1em;
+                                    height: 1em;
+                                    -webkit-font-smoothing: antialiased;
+                                    -moz-osx-font-smoothing: grayscale;
+                                    opacity: 0.6;
+                                    margin-right: 0.25em;
+                                }
+                                .m-button-loader span {
+                                    box-sizing: border-box;
+                                    display: block;
+                                    position: absolute;
+                                    width: 1em;
+                                    height: 1em;
+                                    border: 0.15em solid;
+                                    border-radius: 50%;
+                                    animation: loading-spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+                                    border-color: gray transparent transparent transparent;
+                                }
+                                .m-button-loader span:nth-child(1) {
+                                    animation-delay: -0.45s;
+                                }
+                                .m-button-loader span:nth-child(2) {
+                                    animation-delay: -0.3s;
+                                }
+                                .m-button-loader span:nth-child(3) {
+                                    animation-delay: -0.15s;
+                                }
+                                @keyframes loading-spin {
+                                    0% {
+                                        transform: rotate(0deg);
+                                    }
+                                    100% {
+                                        transform: rotate(360deg);
+                                    }
+                                }
+                            </style>
+                        <script src="/websocket.js"></script><script>_M.controller = 'th-if'; _M.sessionId = '9218172095260071d8265e9ae0483a863ba333e09958c971643'; _M.wsURL = '/socket';_M.wsP = '828b9ea215b08f543460d99e39dc8183f9890a141b8005008ed741e6b336906';</script>
+                        </body>
+                        </html>
+                """;
+
+        final String newHTML = """
+                        <!DOCTYPE html>
+                        <html lang="en">
+                         <head>
+                          <meta charset="UTF-8" />
+                          <title>Th If Problem?</title>
+                          <style>
+                                code {
+                                    color: crimson;
+                                    font-weight: bold;
+                                    padding: .25rem;
+                                }
+                            </style>
+                         </head>
+                         <body>
+                          <section>
+                           <div>
+                            <p>If top is <code>true</code> this should be visible on <code>top</code> of the page</p>
+                           </div>\s
+                            <p>If middle is <code>true</code> this should be visible between <code>top</code> and <code>bottom</code></p>\s
+                          \s
+                           <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code></p>
+                           <div><button onclick="_M.doAction(event, '__FRAGMENT__', `change()`)">change</button>
+                           </div>
+                           <div>
+                            <p>If top is <code>true</code> this should be visible on <code>top</code>, after the button</p>
+                           </div>\s
+                            <p>If middle is <code>true</code> this should be visible between <code>top</code> and <code>bottom</code>, after the button</p>\s
+                          \s
+                           <p>If bottom is <code>true</code> this should be visible at the <code>bottom</code>, after the button</p>
+                          </section>
+                         <div id="m-top-load-bar" class="progress-line" style="display:none;"></div>
+                        <style>
+                        div#m-top-load-bar {
+                            position: fixed;
+                            top: 0;
+                            left: 0;
+                            right: 0;
+                            width: 100%;
+                        }
+                        .progress-line, .progress-line:before {
+                            height: 3px;
+                            width: 100%;
+                            margin: 0;
+                        }
+                        .progress-line {
+                            background-color: #7a00ff;
+                            display: -webkit-flex;
+                            display: flex;
+                        }
+                        .progress-line:before {
+                            background-color: #f4abba;
+                            content: '';
+                            -webkit-animation: running-progress 2s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+                            animation: running-progress 2s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+                        }
+                        @-webkit-keyframes running-progress {
+                            0% { margin-left: 0px; margin-right: 100%; }
+                            50% { margin-left: 25%; margin-right: 0%; }
+                            100% { margin-left: 100%; margin-right: 0; }
+                        }
+                        @keyframes running-progress {
+                            0% { margin-left: 0px; margin-right: 100%; }
+                            50% { margin-left: 25%; margin-right: 0%; }
+                            100% { margin-left: 100%; margin-right: 0; }
+                        }
+                        </style>
+                        <div id="m-full-loader" style="display:none;">Loading ...</div>
+                        <style>
+                        div#m-full-loader {
+                            background: #0000006e;
+                            position: fixed;
+                            top: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 100%;
+                            text-align: center;
+                            padding-top: 15%;
+                        }
+                        </style>
+                        <template id="m-template-button-load">
+                            <span class="m-button-loader"><span></span><span></span><span></span><span></span></span>
+                        </template>
+                        <style>
+                                .m-button-loader {
+                                    display: inline-block;
+                                    position: relative;
+                                    width: 1em;
+                                    height: 1em;
+                                    -webkit-font-smoothing: antialiased;
+                                    -moz-osx-font-smoothing: grayscale;
+                                    opacity: 0.6;
+                                    margin-right: 0.25em;
+                                }
+                                .m-button-loader span {
+                                    box-sizing: border-box;
+                                    display: block;
+                                    position: absolute;
+                                    width: 1em;
+                                    height: 1em;
+                                    border: 0.15em solid;
+                                    border-radius: 50%;
+                                    animation: loading-spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+                                    border-color: gray transparent transparent transparent;
+                                }
+                                .m-button-loader span:nth-child(1) {
+                                    animation-delay: -0.45s;
+                                }
+                                .m-button-loader span:nth-child(2) {
+                                    animation-delay: -0.3s;
+                                }
+                                .m-button-loader span:nth-child(3) {
+                                    animation-delay: -0.15s;
+                                }
+                                @keyframes loading-spin {
+                                    0% {
+                                        transform: rotate(0deg);
+                                    }
+                                    100% {
+                                        transform: rotate(360deg);
+                                    }
+                                }
+                            </style>
+                        <script src="/websocket.js"></script><script>_M.controller = 'th-if'; _M.sessionId = '9218172095260071d8265e9ae0483a863ba333e09958c971643'; _M.wsURL = '/socket';_M.wsP = '828b9ea215b08f543460d99e39dc8183f9890a141b8005008ed741e6b336906';</script>
+                        </body>
+                        </html>
+                """;
+
+        Set<ServerSideDiff> diffs = engine.calculate(oldHTML, newHTML);
+        applyAndTest(oldHTML, newHTML, diffs);
+    }
+
+    @Test
     void testComplex5() {
         String oldHTML = """
                 <section>
